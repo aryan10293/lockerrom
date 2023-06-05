936ca57743fe30497207365355d185a766033737
aondrej ryan <aondrejryan11@gmail.com>
2023-06-05 16:40:44 -0700

    comments now work

--- client/src/pages/CommentSection.tsx
+++ client/src/pages/CommentSection.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Fragment } from 'react'
-import  Comment  from '../components/Comment'
-import DisplayComments from '../components/DisplayComments'
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function CommentSection() {
+    const params = useParams();
+    const id = params.id;
     const [user, setUser] = React.useState<User | null>(null)
+    const [content, setContent] = React.useState<string>('');
+    const [comments, setComments] = React.useState<any[]>([])
     React.useEffect(() => {
         const fetchData = async () => {
         try {
@@ -25,6 +29,55 @@ function CommentSection() {
 
     fetchData();
     }, []);
+    const renderComments = async () => {
+        try {
+            const response = await fetch(`http://localhost:2012/lol/${id}`, {
+            method: 'GET',
+            credentials: 'include',
+            });
+
+            if (response.ok) {
+            const data = await response.json();
+            setComments(data[0].comments);
+            } else {
+            setComments([]);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        }
+    React.useEffect(() => {
+        renderComments()
+    }, [])
+
+    const handleClick = async () => {
+        const obj = {
+            comment: content,
+            date: Date.now(),
+            userId: user?._id,
+            name: user?.userName,
+            featId: id,
+        };
+        try {
+        const response = await fetch(`http://localhost:2012/addcomment/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj),
+        });
+        if (response.ok) {
+            renderComments()
+            // Trigger a callback or update state in the parent component
+        } else {
+            throw new Error('Failed to retrieve comments'); // Throw an error to handle the error case
+        }
+        } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle the error case here
+        }
+  };
+
+
+console.log(comments)
     interface User {
         followers: any[];
         likes: any[];
@@ -38,9 +91,84 @@ function CommentSection() {
     }
   return (
     <>
-        <Comment />
-        <DisplayComments />
-    </>
+    <div className="flex ml-0 sm:mr-0 sm:mx-1 pl-0 pr-1 sm:pr-0 sm:px-1 py-3 border-b">
+      <div className="mt-3 w-12 h-12 text-lg flex-none">
+        <img
+          src={
+            'https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+          }
+          className="flex-none w-12 h-12 rounded-full"
+          alt="avatar"
+        />
+      </div>
+
+      <div className="w-full px-4 py-3 relative">
+        <div className="w-full flex gap-2 justify-between">
+          <h2 className="font-semibold">
+            {`dfej`}
+            <span className="text-slate-600 pl-2">@{'drej'}</span>
+          </h2>
+        </div>
+
+        <div className="flex gap-2">
+          <span className="text-slate-500">replying to</span>
+          <span className="text-blue-600 font-semibold">@{'lebron'}</span>
+        </div>
+
+        {true ? (
+          <div className="flex justify-between items-center mt-3 p-3 px-2 border-y-2 w-full focus:outline-none gap-4">
+            <span className="flex-1">
+              <input
+                value={content}
+                className="w-full p-2 rounded-[30rem] focus:outline-none bg-slate-100"
+                type="text"
+                placeholder="Add a comment..."
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </span>
+
+            <button
+              className="p-2 rounded-[20rem] bg-blue-600 hover:bg-blue-800 text-white shadow-md 
+              hover:shadow-lg w-20"
+              onClick={handleClick}
+            >
+              Comment
+            </button>
+          </div>
+        ) : (
+          <div className="mt-3"></div>
+        )}
+      </div>
+    </div>
+    
+    {comments.map(item => {
+        return (
+            <div className=" bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-5 max-w-md md:max-w-2xl " >
+                <div className="  items-start px-4 py-6">
+                    <div className='flex justify-between'>
+                        <div className='flex'>
+                        <img className=" inline w-12 h-12 rounded-full object-cover mr-4 shadow" src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+                        <div>
+                            <h2 className="flex-1 text-lg font-semibold text-gray-900 -mt-1">{item.name}</h2>
+                            <Link to={`/profile/${item.name}`} className="text-gray-700">{item.name}</Link>
+                        </div>
+                        </div>
+                        <div className="flex inline-block items-center">
+                        <small className="flex-10 text-sm text-gray-700">{item.date} hours ago</small>
+                        </div>   
+                    </div>                                   
+                    <div className="">
+                        <div>
+                        <p className="mt-3 text-gray-700 text-sm">
+                            {item.comment}
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    })}
+</>
   )
 }
 
