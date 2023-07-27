b472f9556cd1b8a6538555603ec8fc651434e15f
aondrej ryan <aondrejryan11@gmail.com>
2023-07-27 00:34:00 -0700

    working on likes page

--- backend/controller/posts.js
+++ backend/controller/posts.js
@@ -96,7 +96,7 @@ module.exports = {
             userLikedPost = userLikedPost[0].likes
             for(let i = 0; i < userLikedPost.length; i++){
                 let likedPost = await Feat.find({_id: userLikedPost[i]})
-                userLikesArray.push(likedPost)
+                userLikesArray.push(likedPost[0])
             }
             console.log(userLikesArray)
             res.send(userLikesArray)
