f9fa7555e01e833065026d21623ca57d641d0d21
aondrej ryan <aondrejryan11@gmail.com>
2023-07-26 22:40:23 -0700

    working on like page

--- backend/controller/posts.js
+++ backend/controller/posts.js
@@ -92,7 +92,7 @@ module.exports = {
     getLikedPost: async (req,res) => {
         try{
             console.log(req.params.id)
-            const userLikedPos = await User.find(req.params.id)
+            const userLikedPos = await User.find({_id: req.params.id})
             console.log(userLikedPos)
         } catch (error) {
             console.error('Error fetching user:', error);
